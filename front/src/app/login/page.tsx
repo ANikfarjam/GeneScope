440030ae6be41416f6753827cac9586e485fbd43
khmorad <khakho.morad@gmail.com>
2025-02-13 12:06:21 -0800

    fixed routing issue

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { login } from "../auth";
 import Link from "next/link";
 import "../styles/login.css"; // Import CSS for styling
