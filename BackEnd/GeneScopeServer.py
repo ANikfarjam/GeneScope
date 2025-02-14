1952733678038af762e5e4136b389c945797c33a
Ashkan Nikfarjam <ashkan_nikfarjam@yahoo.com>
2025-02-14 04:33:15 -0800

    added data insight, flask api, created route for user data classification

---
+++ BackEnd/GeneScopeServer.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from routers.UserDataClassificationRoute import classification_blueprint
+app = Flask(__name__)
+# allow universal requests
+CORS(app, resources={
+    #r"/api/*": {"origins": "http://localhost:3000"},
+    r"/*": {"origins": "http://localhost:3000"}
+})
+#register routes
+app.register_blueprint(classification_blueprint)
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
