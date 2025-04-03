7f34bd4317520b1af4a449d3ecbb0cb46080f9d2
khmorad <khakho.morad@gmail.com>
2025-04-02 18:07:30 -0700

    imporved on pinecone scripts

---
+++ front/src/app/lib/embedAndStore.ts
@@ -0,0 +1,33 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/community/vectorstores/pinecone";
+import { pineconeClient } from "./pinecone";
+
+export async function storePdfsInPinecone() {
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.NEXT_PUBLIC_STUFF,
+  });
+  const stagingDocs = await new PDFLoader("data/staging.pdf").load();
+  const hmmDocs = await new PDFLoader("data/hmm.pdf").load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+
+  const allDocs = [
+    ...(await splitter.splitDocuments(stagingDocs)),
+    ...(await splitter.splitDocuments(hmmDocs)),
+  ];
+
+  try {
+    await PineconeStore.fromDocuments(allDocs, embeddings, {
+      pineconeIndex: pineconeClient.Index("gene-scope"),
+    });
+
+    console.log("PDF embeddings stored in Pinecone.");
+  } catch (error) {
+    console.error("Error storing PDF embeddings in Pinecone:", error);
+  }
+}
