9aa937881b594f45b07a9ba22b79bbaaac7af305
khmorad <khakho.morad@gmail.com>
2025-04-02 19:09:24 -0700

    fix script

--- front/src/app/lib/pinecone.ts
+++ front/src/app/lib/pinecone.ts
@@ -1,8 +1,7 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 
 const pineconeClient = new Pinecone({
-  apiKey:
-    "pcsk_6cNGsD_PH5j5fHSHxyyCA33fghRxC5TxBwGE9sQjHZboUdVFhzyQp8CmW7s3mLJHwHsKxW",
+  apiKey: process.env.PINECONE_API_KEY || "",
 });
 
 export { pineconeClient };
